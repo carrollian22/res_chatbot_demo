7ddd364f98c6e190617f798619559a3d4365236c
carrollian22 <ic.analytics22@gmail.com>
2025-06-17 16:04:28 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 def build_qa_chain(vector_store):
     llm = ChatOpenAI(model="gpt-4o", temperature=0)
